b5d669cea4ff1843977690103f65b95a3b6c6a10
Peter <magic@kthx.at>
2023-07-11 18:45:48 +0200

    Updating files to latest working state

--- ircporn/ircporn.py
+++ ircporn/ircporn.py
@@ -1,34 +1,4 @@
 #!/usr/bin/env python3
-# Copyright (c) 2016, Cyril Roelandt
-# Reupload and edited  by Peter Stanke (MAGIC), https://git.kthx.at/MAGIC/PornServ
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are
-# met:
-#
-# 1. Redistributions of source code must retain the above copyright
-# notice, this list of conditions and the following disclaimer.
-#
-# 2. Redistributions in binary form must reproduce the above copyright
-# notice, this list of conditions and the following disclaimer in the
-# documentation and/or other materials provided with the distribution.
-#
-# 3. Neither the name of the copyright holder nor the names of its
-# contributors may be used to endorse or promote products derived from
-# this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
-# IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
-# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
-# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-# HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
-# TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import argparse
 import itertools
@@ -131,18 +101,18 @@ def main():
     browsers.append(RedditBrowser(subreddits))
 
     irc3.IrcBot.from_config({
-        'nick': config['nick'],
-        'realname': config['realname'],
-        'username': config['username'],
-        'autojoins': config['channels'],
-        'host': config['server'],
+	    'host': config['server'],
         'port': config['port'],
-        'ssl': True,
-        'ssl_verify': 'CERT_NONE',
+		'ssl': config['ssl'],
+        'ssl_verify': config['ssl_verify'],
+		'autojoins': config['channels'],
+        'nick': config['nick'],
+		'username': config['username'],
+        'realname': config['realname'],
+		'sasl_username': config['sasl_username'],
+        'sasl_password': config['sasl_password'],
         'verbose': True,
-        'includes': [__name__],
-        'sasl_username': config['sasl_username'],
-        'sasl_password': config['sasl_password']
+        'includes': [__name__]
     }).run()
 
 if __name__ == '__main__':
